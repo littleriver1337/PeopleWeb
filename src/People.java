295ca83a1845bd3c0928f61f82cd6053ec598bdf
MattBrown <litteriver1337@gmail.com>
2015-10-28 15:29:06 -0400

    getting there

--- src/People.java
+++ src/People.java
@@ -1,6 +1,11 @@
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by zach on 10/19/15.
@@ -20,8 +25,36 @@ public class People {
             Person person = new Person(Integer.valueOf(columns[0]), columns[1], columns[2], columns[3], columns[4], columns[5]);
             people.add(person);
         }
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    String offset = request.queryParams("offset");
+                    int offsetNum;
+                    if (offset == null){
+                        offsetNum = 0;
+                    }else {
+                        offsetNum = Integer.valueOf(offset);
+                    }
+                    ArrayList<Person> tempList = new ArrayList(people.subList(offsetNum, offsetNum + 20));
+                    HashMap m = new HashMap();
+                    m.put("people", tempList);
+                    m.put("offset", offsetNum + 20);
+                    return new ModelAndView(m, "people.html");
+                }),
+                new MustacheTemplateEngine()
+        );
+        Spark.get(
+                "/person",
+                ((request, response) -> {
+                    HashMap newM = new HashMap();
+                    newM.put("person", people);
+                    return new ModelAndView(newM, "person.html");
 
-        // write Spark route here
+                    //response.redirect("/");
+                   //return ""
+                }),
+                new MustacheTemplateEngine()
+        );
     }
 
     static String readFile(String fileName) {
