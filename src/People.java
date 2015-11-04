d7e397f5383cef8b2419e148abc2f8390c891dde
MattBrown <litteriver1337@gmail.com>
2015-11-04 14:47:26 -0500

    added insertPerson and selectPerson

--- src/People.java
+++ src/People.java
@@ -4,6 +4,7 @@ import spark.template.mustache.MustacheTemplateEngine;
 
 import java.io.File;
 import java.io.FileReader;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -11,12 +12,40 @@ import java.util.HashMap;
  * Created by zach on 10/19/15.
  */
 public class People {
-    public static void createTables(){
-
+    public static void createTables(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("DROP TABLE IF EXISTS people");
+        stmt.execute("CREATE TABLE people (id IDENTITY, first_name VARCHAR, last_name VARCHAR," +
+                " email VARCHAR, country VARCHAR, ip VARCHAR)");
+    }
+    public static void insertPerson(Connection conn, String firstName, String lastName, String email,
+                                    String country, String ip) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO people VALUES (NULL, ?, ?, ?, ?, ?)");
+        stmt.setString(1, firstName);
+        stmt.setString(2, lastName);
+        stmt.setString(3, email);
+        stmt.setString(4, country);
+        stmt.setString(5, ip);
+        stmt.execute();
+    }
+    public static Person selectPerson(Connection conn, int id) throws SQLException {
+        Person person = null;
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM people WHERE id = ?");
+        stmt.setInt(1, id);
+        ResultSet results = stmt.executeQuery();
+        if(results.next()){
+            person = new Person();
+            person.id = results.getInt("id");
+        }
+        return person;
     }
 
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+
         ArrayList<Person> people = new ArrayList();
 
         final int SHOW_COUNT = 20;
---
+++ src/test/PeopleTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by MattBrown on 11/4/15.
+ */
+public class PeopleTest {
+    public Connection startConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./test");
+        People.createTables(conn);
+        return conn;
+    }
+    public void endConnection(Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("DROP TABLE people");
+        conn.close();
+    }
+    @Test
+    public void testPerson() throws SQLException {
+        Connection conn = startConnection();
+        People.insertPerson(conn, "Matt", "Brown", "littlriver1337@gmail.com", "Alabama", "91000");
+        Person person = People.selectPerson(conn, 1);
+
+        endConnection(conn);
+
+        assertTrue(person != null);
+    }
+}
